3c931d1442b94ab18963a749b418139c1ca7a319
pmgallardo <pmgallardo@usal.es>
2024-08-28 22:55:35 +0200

    Topics added to call 2022 exams of CSSTIAE

--- src/quiz.py
+++ src/quiz.py
@@ -1,3 +1,4 @@
 from quiz.quiz_cli.quiz_cli import quiz_run
 
-quiz_run()
+if __name__ == "__main__":
+    quiz_run()
--- src/quiz/quiz_cli/quiz_cli.py
+++ src/quiz/quiz_cli/quiz_cli.py
@@ -3,6 +3,7 @@ import re
 from utils.text_utils import number_to_letter
 from utils.text_utils import letter_to_number
 from utils.text_utils import dbgt
+from utils.text_utils import printt
 
 # Width of the terminal where text is going to be displayed
 TERM_WIDTH = 80
@@ -32,11 +33,11 @@ class QuizQuestion:
     def get_statement_and_options_text(self):
         text = self.statement + "\n\n"
         for i, option in enumerate(self.options):
-            opt_letter = self.number_to_letter(i+1)
+            opt_letter = number_to_letter(i+1)
             text += f"{opt_letter}) {option} \n"
         return text
 
-class AGTQuizQuestion(QuizQuestion):
+class AGEQuizQuestion(QuizQuestion):
     def __init__(self):
         super().__init__()
 
@@ -80,22 +81,57 @@ class AGTQuizQuestion(QuizQuestion):
             return body
 
 class QuizQuestionPart:
-    def __init__(self, questions, part_type):
+    def __init__(self, questions, type):
         self.questions = questions
-        # example: general or specific questions
-        self.part_type = part_type
+        # type example: general or specific questions
+        self.type = type
+
+    def get_text(self):
+        text = ""
+        for i, q in enumerate(self.questions):
+            text += "Pregunta anulada = " + str(q.is_cancelled) + "\n"
+            text += "Pregunta reserva = " + str(q.is_reserve) + "\n"
+            text += "Tema = " + str(q.topic) + "\n"
+            text += "#" + str(i+1) + " "
+            text += q.statement + "\n"
+            for j, option in enumerate(q.options):
+                text += number_to_letter(j+1) + ") " + option + "\n"
+            text += "Respuesta correcta: " + number_to_letter(q.correct_answer_no) + "\n"
+            text += "\n"
+        text += "\n"
+        return text
 
 class QuizQuestionGroup:
     def __init__(self, parts):
-        self.parts = []
+        self.parts = parts
+
+        self.body = None
+        self.call = None
+        self.source = None
+        self.date = None
+        self.date = None
+
+    def append_part(self, part):
+        self.parts.append(part)
+
+    def get_text(self):
+        text = ""
+        for i, part in enumerate(self.parts):
+            text += "PARTE " + str(i+1) + "\n\n"
+            text += part.get_text()
+        return text
 
 class QuizQuestionPool:
     def __init__(self):
         self.question_groups = []
 
-        self.browse_exams()
+        self.load_exams()
 
-    def browse_exams(self):
+    def load_exams(self):
+        aiken_filepaths = self.scrawl_aiken_exams()
+        self.parse_aiken_exams(aiken_filepaths)
+
+    def scrawl_aiken_exams(self):
         # fullpath = "../../../data/processed/1166/test/2020/20220514/1166_2020_20220514_esp.aiken"
         # fullpath = "../../../data/processed/1166/test/2020/20220514/1166_2020_20220514_gen.aiken"
         # fullpath = "../../../data/processed/1166/test/2020/20220527/1166_2020_20220527_esp.aiken"
@@ -105,9 +141,20 @@ class QuizQuestionPool:
         # fullpath = "../../../data/processed/1166/test/2022/20231026/1166_2022_20231026_esp.aiken"
         # fullpath = "../../../data/processed/1166/test/2022/20231026/1166_2022_20231026_gen.aiken"
 
-        filepaths = [ "../data/processed/1166/test/2022/20231026/1166_2022_20231026_gen.aiken" ]
+        filepaths = ["../data/processed/1166/test/2022/20231006/1166_2022_20231006_esp.aiken"]
+        #filepaths = [ "../data/processed/1166/test/2022/20231026/1166_2022_20231026_gen.aiken" ]
 
-        for filepath in filepaths:
+
+        return filepaths
+
+    def parse_aiken_exams(self, aiken_filepaths):
+        body = "1166"
+        call = "2022"
+        source = "agt"
+        date = "20231026"
+        mode = None
+
+        for filepath in aiken_filepaths:
             # Read file and extract questions
             file = open(filepath, "r")
             lines = file.readlines()
@@ -116,15 +163,21 @@ class QuizQuestionPool:
             # Parse AIKEN files
             questions = self.parse_aiken_exam(lines)
             for question in questions:
-                question.body = "1166"
-                question.call = "2022"
-                question.agt = "agt"
-                question.date = "20231026"
-                question.mode = ""
+                question.body = body
+                question.call = call
+                question.source = source
+                question.date = date
+                question.mode = mode
 
             question_part = QuizQuestionPart(questions, "")
+
             # A question group could be an exam or other group of questions
             question_group = QuizQuestionGroup([question_part])
+            question_group.body = body
+            question_group.call = call
+            question_group.source = source
+            question_group.mode = mode
+            question_group.date = date
             self.question_groups.append(question_group)
 
     def parse_aiken_exam(self, lines):
@@ -139,7 +192,7 @@ class QuizQuestionPool:
             if line != "" or question is not None:
                 # If question is not started
                 if question is None:
-                    question = AGTQuizQuestion()
+                    question = AGEQuizQuestion()
                     question.num = i+1
                     cur_q_item = line.rstrip()
                     dbgt(" > Save initial text: " + cur_q_item)
@@ -271,12 +324,210 @@ class QuizQuestionPool:
             number = int(match.group(1))
 
             # Remove the matched substring from the input string
-            result_string = re.sub(pattern, '', input_string, count=1).strip()
+            result_string = re.sub(pattern, '', statement, count=1).strip()
 
             return number, result_string
         else:
             # If no match is found, return None and the original string
             return None, statement
+
+    def get_quiz_by_exam(self, body_id, call, mode, date):
+        quiz = AGEQuiz()
+
+        for q_group in self.question_groups:
+            q_group
+
+        return quiz
+
+    def get_quiz_by_topic(self, body, topic):
+        quiz_parts = []
+        quiz_questions = []
+
+        # Add questions of topics from pool that matches selection
+        for q_groups in self.question_groups:
+            if body == q_groups.body:
+                for part in q_groups.parts:
+                    for k, q in enumerate(part.questions):
+                        if q.topic is not None:
+                            if topic == q.topic:
+                                quiz_questions.append(q)
+
+        part = QuizQuestionPart(quiz_questions, None)
+        quiz_parts.append(part)
+        q_group =QuizQuestionGroup(quiz_parts)
+        quiz = AGEQuiz(q_group)
+        return quiz
+
+    def get_quiz_by_block(self, body_id, block):
+        quiz = AGEQuiz()
+        return quiz
+
+class AGEQuiz:
+    def __init__(self, q_group):
+        self.q_group = q_group
+
+    def get_text_questions(self):
+        return self.q_group.get_text()
+
+        # option_char = ")"
+        #
+        # text_qs = ""
+        # for i, question in enumerate(self.questions):
+        #     # text_qs += "STATEMENT "
+        #     text_qs += "#" + str(i + 1) + " "
+        #     text_qs += question.statement + "\n"
+        #     for j, option in enumerate(question.options):
+        #         text_qs += self.number_to_letter(j + 1) + option_char + " "
+        #         text_qs += option + "\n"
+        #
+        #     text_qs += "ANSWER: "
+        #     answer = question.correct_answer_no
+        #     if answer != "":
+        #         text_qs += self.number_to_letter(answer).upper()
+        #     text_qs += "\n"
+        #     if i < len(self.questions) - 1:
+        #         text_qs += "\n"
+        # return text_qs
+
+    def play(self):
+        # init counters
+        num_cancelled_qs = []
+        num_reserve_qs = []
+        num_correct_answers = []
+        num_incorrect_answers = []
+
+
+        for i, part in enumerate(self.q_group.parts):
+            # init counters
+            num_cancelled_qs.append(0)
+            num_reserve_qs.append(0)
+            num_correct_answers.append(0)
+            num_incorrect_answers.append(0)
+
+            questions = part.questions
+
+            printt("PARTE " + str(i+1) + "\n")
+
+            for j, question in enumerate(questions):
+                # Loop all questions
+                    # Check whether question is cancelled, has no answer os is reserve
+                    if question.is_cancelled:
+                        num_cancelled_qs[i] += 1
+                        printt("PREGUNTA ANULADA")
+                    else:
+                        if not question.has_answer:
+                            num_cancelled_qs[i] += 1
+                            printt("PREGUNTA SIN RESPUESTA, SE CONSIDERA NULA")
+                        elif question.is_reserve:
+                            num_reserve_qs[i] += 1
+
+                    # Display statement
+                    printt("\nPREGUNTA " + str(j + 1) + "/" + str(len(questions)))
+
+                    # Display options
+                    printt(question.get_statement_and_options_text())
+
+                    if question.has_answer:
+                        # Prompt answer
+                        player_option = input("Introduzca respuesta [A-D]: ")
+
+                        player_option_no = letter_to_number(player_option)
+                        correct_option_no = question.correct_answer_no
+                        # Check answer
+                        if player_option_no == correct_option_no:
+                            is_useless_reserve = question.is_reserve and (num_cancelled_qs[i] < num_reserve_qs[i])
+
+                            if not (is_useless_reserve or question.is_cancelled):
+                                # Add correct answer
+                                num_correct_answers[i] += 1
+                            printt("¡Correcto!")
+                        else:
+                            is_useless_reserve = question.is_reserve and (num_cancelled_qs[i] < num_reserve_qs[i])
+
+                            if not (is_useless_reserve or question.is_cancelled):
+                                # Add incorrect answer
+                                num_incorrect_answers[i] += 1
+
+                            correct_option_letter = number_to_letter(correct_option_no).upper()
+                            printt("Incorrecto. La respuesta correcta era " + correct_option_letter + ".")
+
+        # Display total result
+
+        # values published after the correction of each test
+        #min_direct_score = 39 # 2022 L ord
+        #min_direct_score = 39 # 2022 L ext
+        #min_direct_score = 37.2 # 2022 L ord
+        #min_direct_score = 39 # 2022 L ext
+        #min_direct_score = 9  # 2022 L ext ESP
+        #min_direct_score = 30  # 2022 L ext GEN
+
+
+
+        for i, part in enumerate(self.q_group.parts):
+
+            print("RESULTADOS PARTE " + str(i+1) + "\n")
+
+            questions = part.questions
+
+            # maximum direct score should remove cancelled answers
+            num_total_qs = len(questions)
+            num_used_reserved_qs = self.calc_num_used_reserve_qs(num_reserve_qs[i], num_cancelled_qs[i])
+            num_valid_qs = self.calc_num_valid_qs(num_total_qs, num_reserve_qs[i], num_cancelled_qs[i])
+            max_direct_score = num_valid_qs
+
+            #prop_min_direct_score = min_direct_score / max_direct_score
+            prop_min_direct_score = 0.30
+            min_direct_score = prop_min_direct_score * max_direct_score
+
+            # Score calculation
+            direct_score = num_correct_answers[i] - (num_incorrect_answers[i] / 3)
+            prop_direct_score = direct_score / max_direct_score
+            ## If direct score is negative, it is converted to zero
+            #if direct_score < 0:
+            #    direct_score = 0
+
+            direct_score_2dec = "{:.2f}".format(round(direct_score, 2))
+            per_direct_score_2dec = "{:.2f}".format(round(prop_direct_score*100, 2))
+            per_min_direct_score_2dec = "{:.2f}".format(round(prop_min_direct_score*100, 2))
+
+            unanswered_questions = num_valid_qs - num_correct_answers[i] - num_incorrect_answers[i]
+            printt("\n¡Fin del exámen!\n")
+            printt(f"Preguntas totales: {num_total_qs}")
+            printt(f"Preguntas anuladas: {num_cancelled_qs[i]}")
+            printt(f"Preguntas de reserva (usadas y sin usar): {num_reserve_qs[i]}")
+            printt(f"Preguntas de reserva usadas: {num_used_reserved_qs}")
+            printt(f"Preguntas totales válidas: {num_valid_qs}")
+            printt(f"Preguntas acertadas: {num_correct_answers[i]}")
+            printt(f"Preguntas erradas: {num_incorrect_answers[i]}")
+            printt(f"Preguntas sin contestar: {unanswered_questions}\n")
+
+            printt(f"Tu puntuación directa: {direct_score_2dec}")
+            printt(f"Has obtenido un {per_direct_score_2dec} % de la máxima puntuación respecto al {per_min_direct_score_2dec} % mínimo exigido.")
+
+            if direct_score < min_direct_score:
+                print("Lo siento, no habrías aprobado el ejercicio.")
+            else:
+                print("Enhorabuena, ¡habrías aprobado el ejercicio!")
+
+            ## Calculation of transformed score
+            #if direct_score < min_direct_score:
+            #    transformed_score = max_transformed_score / 2 * direct_score / min_direct_score
+            #else:
+            #    transformed_score = max_transformed_score / 2 * (1 + (direct_score - min_direct_score) / (max_direct_score - min_direct_score)
+
+    def calc_num_used_reserve_qs(self, reserve_questions, cancelled_questions):
+        if reserve_questions <= cancelled_questions:
+            return reserve_questions
+        else:
+            return cancelled_questions
+
+    def calc_num_valid_qs(self, num_total_qs, num_reserve_qs, num_cancelled_qs):
+        if num_reserve_qs >= num_cancelled_qs:
+            # Reserved are ignored as they have substituted all cancelled questions
+            return num_total_qs - num_reserve_qs
+        else:
+            # Cancelled are ignored as the reserved questions are less
+            return num_total_qs - num_cancelled_qs
 
 class QuizConf:
     def __init__(self):
@@ -477,6 +728,18 @@ class Quiz:
         self.questions = vquestions
 
     def play(self):
+        # for each question
+        # Display statement
+
+        # Display options
+
+        # Prompt answer
+
+        # Check answer
+
+        # Display total result
+
+
         self.load_questions()
 
         # values published after the correction of each test
@@ -557,16 +820,60 @@ def print_title():
         # Read the contents of the file
         print(file.read() + '\n')
 
-def quiz_run():
-    # load_question_pool
-    question_pool = QuizQuestionPool()
 
-    # Display title
-    print_title()
+def select_and_play_quizzes(question_pool):
+    # quiz loop
+    user_wants_another_quiz = True
+    while user_wants_another_quiz:
+        # Propose default configuration
+        default_conf_input = input("¿Quieres comenzar con la configuración por defecto? [S/N] (defecto: S): ")
 
-    #quiz loop
-    question_pool[0][0]
+        if default_conf_input == 'S' or default_conf_input == '':
+            pass
+            # # Load default configuration
+            # quiz.conf.body_id = 1166
+            # quiz.conf.practice_type = 'E'
+            # quiz.conf.exam_no = 1
+        else:
+            # Read all Spain's AGE bodies
 
+            # Prompt body ID
+            body_id = input("Introduza código de cuerpo: (defecto: 1166)")
+            if quiz.conf.body_id == '':
+                quiz.conf.body_id = 1166
+
+            # Given the body path, loop all files containing .aiken extension
+
+            # Read list topics from latest call
+
+            # Prompt practice type
+            practice_type = input("Indique si quiere practicar ejercicio [E] o tema [T]: (defecto: E)")
+            if practice_type == '':
+                practice_type = 'E'
+
+            if practice_type == 'E':
+                # List available exams
+
+                # Prompt exam ordinal number
+                exam_no = input("Introduzca número de exámen: (defecto: el primero): ")
+
+            elif practice_type == 'T':
+                # List topics
+
+                # Prompt topic number
+                pass
+                topic_no = input("Introduzca tema: (defecto: 1): ")
+
+            else:
+                print("\nPráctica desconocida")
+
+        another_exam_input = input("¿Quieres realizar otro exámen? [S/N] (defecto S): ").strip().upper()
+        if another_exam_input == 'N':
+            user_wants_another_quiz = False
+
+
+
+def select():
     # Propose default configuration
     quiz = Quiz()
 
@@ -580,17 +887,14 @@ def quiz_run():
     else:
         # Read all Spain's AGE bodies
 
-
         # Prompt body ID
         body_id = input("Introduza código de cuerpo: (defecto: 1166)")
-        if  quiz.conf.body_id == '':
+        if quiz.conf.body_id == '':
             quiz.conf.body_id = 1166
 
         # Given the body path, loop all files containing .aiken extension
 
-
         # Read list topics from latest call
-
 
         # Prompt practice type
         practice_type = input("Indique si quiere practicar ejercicio [E] o tema [T]: (defecto: E)")
@@ -613,28 +917,63 @@ def quiz_run():
         else:
             print("\nPráctica desconocida")
 
+def play():
 
     # Ask questions
     quiz.play()
 
-    #for each question
-        # Display statement
+def quiz_run():
 
-        # Display options
+    # Question pool load
+    question_pool = QuizQuestionPool()
 
-        # Prompt answer
+    #exam = question_pool.question_groups[0]
+    #printt(exam.get_text())
 
-        # Check answer
+    print_title()
 
-        # Display total result
+    # Quiz selection
 
 
-        # Ask if it wants to perform another test
+    # Quiz preparation
+
+    body = "1166"
+
+    call = "2022"
+    mode = "l"
+    date = "20231006"
+
+    topic_no = 108
+
+    block = "e"
+
+    # quiz = question_pool.get_quiz_by_exam(body_id, call, mode, date)
+    quiz = question_pool.get_quiz_by_topic(body, topic_no)
+    # quiz = question_pool.get_quiz_by_block(body_id, block)
+
+    printt(quiz.get_text_questions())
+
+    # Call quiz
+    quiz.play()
+
+    #select_and_play_quizzes(question_pool)
+
+    # Old functionality
+    # Display title
+    #print_title()
+
+    #select()
+    #play()
+
+    # Ask if it wants to perform another test
+
 
 def main():
     quiz_run()
 
-main()
+if __name__ == "__main__":
+    main()
+
 
 
 
